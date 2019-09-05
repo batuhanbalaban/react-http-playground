aef447946d5f9ac5d5e52e1c9ef13f5f0b7c99a1
unknown <batuhan.balaban@yahoo.com>
2019-09-05 20:31:13 +0300

    catch http errors with axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ class Blog extends Component {
 
     state = {
         posts:[],
-        selectedPostId:null
+        selectedPostId:null,
+        error:false
     }
 
     componentDidMount(){
@@ -24,6 +25,10 @@ class Blog extends Component {
             })
             this.setState({posts:updatedPosts});
             //console.log(response);
+        })
+        .catch(error =>{
+            this.setState({error:true});
+            console.log(error);
         });
     }
     postSelectedHandler = (id) =>{
@@ -31,16 +36,18 @@ class Blog extends Component {
     }
     render () {
 
-        const posts=this.state.posts.map(
-            post=>{
-                return <Post 
-                            key={post.id} 
-                            title={post.title} 
-                            author={post.author}
-                            clicked={()=>this.postSelectedHandler(post.id)}/>
-            }
-        );
-
+        let posts = <p style={{textAlign:'center'}}>Something went wrong!</p>
+        if(!this.state.error){
+            posts=this.state.posts.map(
+                post=>{
+                    return <Post 
+                                key={post.id} 
+                                title={post.title} 
+                                author={post.author}
+                                clicked={()=>this.postSelectedHandler(post.id)}/>
+                }
+            );
+        }
 
 
         return (
