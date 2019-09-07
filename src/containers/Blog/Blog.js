c5139f827828fd826116b62c31c3ade41fabfb29
unknown <batuhan.balaban@yahoo.com>
2019-09-07 20:59:41 +0300

    Redirect

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import {Route,NavLink,Switch}from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect}from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
 
@@ -18,7 +18,7 @@ class Blog extends Component {
                             activeStyle={{
                                 color:'#fa923f',
                                 textDecoration:'underline'
-                            }}>Home</NavLink></li>
+                            }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash:'#submit',
@@ -31,9 +31,11 @@ class Blog extends Component {
                 
                 {/* IF ROUTES WRAPPED WITH Switch component then only first Route component will be rendered,  */}
                 <Switch>
-                    <Route path="/" exact component={Posts}/>
-                    <Route path="/new-post" exact component={NewPost}/>
                     <Route path="/posts/:id" exact component={FullPost}/>
+                    <Route path="/posts" exact component={Posts}/>
+                    <Route path="/new-post" exact component={NewPost}/>
+                    {/* <Route path="/" exact component={Posts}/> */}
+                    <Redirect from="/" to="/posts"  />
                 </Switch>
 
                 
